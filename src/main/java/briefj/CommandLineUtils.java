f524b6c394a4f046984b863d8eb10475e65d5e0b
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-01-05 10:46:23 -0800

    Minor changes in CommandLineUtils

--- src/main/java/briefj/CommandLineUtils.java
+++ src/main/java/briefj/CommandLineUtils.java
@@ -6,8 +6,25 @@ import com.beust.jcommander.JCommander;
 
 public class CommandLineUtils
 {
+  /* TODO: need to modify JCommander:
+   * 
+   * - remove the name argument, redundant
+   * - default values as textual argument, output them to property file
+   * - create descent usage
+   * - create briefer sub-command
+   * 
+   * Idea: experiments launched via vagrant style configs?
+   * just detect when there is a new run file?
+   * integrate workflow language with decent property file format?
+   */
+  
   public static void start(Runnable runnable, String [] args)
   {
+    if (args.length > 0  && args[0].matches("[-][-]?help"))
+    {
+      new JCommander(runnable).usage();
+      System.exit(0);
+    }
     new JCommander(runnable, args);
     runnable.run();
   }
