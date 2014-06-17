0f752ad9edcdbb796305edc67c245cbc7ce79de1
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-06-16 17:53:40 -0700

    Should have been included in previous commit

--- src/main/java/briefj/DefaultCharset.java
+++ src/main/java/briefj/DefaultCharset.java
@@ -1,5 +1,9 @@
 package briefj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 
@@ -14,4 +18,9 @@ import com.google.common.base.Charsets;
 public class DefaultCharset
 {
   public static Charset defaultCharset = Charsets.UTF_8;
+  
+  public static BufferedReader getReader(InputStream in)
+  {
+    return new BufferedReader(new InputStreamReader(in, defaultCharset));
+  }
 }
